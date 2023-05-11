830b55aaf5f1c85748aea2332b3844a90ebae0c9
Manank Patni <manank321@gmail.com>
2023-05-11 23:19:09 +0530

    Fix web3
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- src/components/SubdomainItem.jsx
+++ src/components/SubdomainItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import Web3Service from "../utils/web3";
 import ShowBuyingModal from "./ShowBuyingModal";
 
-const SubdomainItem = props => {
+const SubdomainItem = (props) => {
   const [openBuying, setOpenBuying] = React.useState(false);
   const buySubdomain = async () => {
     if (props.buyable) {
